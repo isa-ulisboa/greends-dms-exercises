52bc6ed2c2bbf71257db1af411e15abd77edb739
inesbs <isilveira@edu.ulisboa.pt>
2022-12-03 14:17:07 +0000

    web scraping attempt

---
+++ final_assignment/web_scrap.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd 
+
+"""
+# open the html file
+with open("final_assignment\strawberries_html.html", "r", encoding="utf-8") as f:
+  doc = BeautifulSoup(f, "html.parser")
+
+#print(doc.prettify())
+
+tags = doc.find_all("strong")
+print(tags)
+
+"""
+import requests
+
+url = "https://www.healthline.com/nutrition/foods/strawberries#nutrition"
+
+result = requests.get(url)
+doc = BeautifulSoup (result.text, "html.parser")
+#print(doc.prettify())
+
+nutritional_values = doc.find_all("strong")
+print(nutritional_values)
